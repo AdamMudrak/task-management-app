58a17206e280dffd2ee6fdafe92371d34faf1cc6
Adam Mudrak <mudrets2000@gmail.com>
2025-04-15 23:41:42 +0300

    Add user and role and their constants

---
+++ src/main/java/com/example/taskmanagementapp/constants/EntitiesConstants.java
@@ -0,0 +1,12 @@
+package com.example.taskmanagementapp.constants;
+
+public class EntitiesConstants {
+
+    public static final String USERS = "users";
+    public static final String USERS_ROLES_JOIN_TABLE = "users_roles";
+    public static final String USER_ID = "user_id";
+
+    public static final String ROLES = "roles";
+    public static final String ROLE_ID = "role_id";
+    public static final String BOOLEAN_TO_INT = "TINYINT(1)";
+}
