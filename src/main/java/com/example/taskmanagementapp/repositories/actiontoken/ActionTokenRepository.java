cef7ad269818e0c8c5d3c447f0eb1d5abe50b906
Adam Mudrak <mudrets2000@gmail.com>
2025-04-16 20:46:03 +0300

    Delete ActionTokenRepository.java as unneeded so far

--- src/main/java/com/example/taskmanagementapp/repositories/actiontoken/ActionTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.taskmanagementapp.repositories.actiontoken;
-
-import com.example.taskmanagementapp.entities.tokens.ActionToken;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActionTokenRepository extends JpaRepository<ActionToken, Long> {
-    Optional<ActionToken> findByActionToken(String actionToken);
-}
