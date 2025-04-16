3fb4e1d8535dbcf642fe268b0c69eec78e3b0d5f
Adam Mudrak <mudrets2000@gmail.com>
2025-04-16 15:40:41 +0300

    Delete unneeded annotation

--- src/main/java/com/example/taskmanagementapp/dtos/authentication/request/UserLoginRequestDto.java
+++ src/main/java/com/example/taskmanagementapp/dtos/authentication/request/UserLoginRequestDto.java
@@ -3,7 +3,6 @@ package com.example.taskmanagementapp.dtos.authentication.request;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import com.example.taskmanagementapp.constants.dtos.UserDtoConstants;
-import com.example.taskmanagementapp.validation.email.Email;
 import com.example.taskmanagementapp.validation.password.Password;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,7 +14,6 @@ public record UserLoginRequestDto(
         example = UserDtoConstants.EMAIL_OR_USERNAME_EXAMPLE,
         requiredMode = REQUIRED)
         @NotBlank
-        @Email
         String emailOrUsername,
 
         @Schema(name = UserDtoConstants.PASSWORD,
