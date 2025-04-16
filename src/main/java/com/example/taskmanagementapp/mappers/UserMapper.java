02a7c8af6e53f51f90fb6be748511fcdbd3d65a9
Adam Mudrak <mudrets2000@gmail.com>
2025-04-16 15:38:15 +0300

    Add toUser for UserMapper for Registration process

--- src/main/java/com/example/taskmanagementapp/mappers/UserMapper.java
+++ src/main/java/com/example/taskmanagementapp/mappers/UserMapper.java
@@ -1,8 +1,11 @@
 package com.example.taskmanagementapp.mappers;
 
 import com.example.taskmanagementapp.config.MapperConfig;
+import com.example.taskmanagementapp.dtos.authentication.request.UserRegistrationRequestDto;
+import com.example.taskmanagementapp.entities.User;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
+    User toUser(UserRegistrationRequestDto requestDto);
 }
