c057cb5e17f94e672f5554efdea9a1f8a9dfea12
Adam Mudrak <mudrets2000@gmail.com>
2025-04-16 13:03:55 +0300

    Add basic email Service and its Password Reset extension

---
+++ src/main/java/com/example/taskmanagementapp/security/email/EmailService.java
@@ -0,0 +1,42 @@
+package com.example.taskmanagementapp.security.email;
+
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.SEND_GRID_API_KEY;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.SUPPORT_EMAIL;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Value(SUPPORT_EMAIL)
+    protected String supportEmail;
+
+    @Value(SEND_GRID_API_KEY)
+    private String sendGridApiKey;
+
+    public void sendMessage(String toEmail, String subject, String body) {
+        Email from = new Email(supportEmail);
+        Email to = new Email(toEmail);
+        Content content = new Content("text", body);
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sendGrid = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sendGrid.api(request);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/example/taskmanagementapp/security/email/PasswordEmailService.java
@@ -0,0 +1,68 @@
+package com.example.taskmanagementapp.security.email;
+
+import static com.example.taskmanagementapp.constants.Constants.SPLITERATOR;
+import static com.example.taskmanagementapp.constants.redirects.RedirectConstants.RANDOM_PASSWORD_REDIRECT_LINK;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.CONFIRMATION;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.CONFIRMATION_PATH;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.CONFIRM_REGISTRATION_BODY;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.CONFIRM_REGISTRATION_SUBJECT;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.INITIATE_RANDOM_PASSWORD_BODY;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.INITIATE_RANDOM_PASSWORD_SUBJECT;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.RANDOM_PASSWORD_BODY;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.RANDOM_PASSWORD_BODY_2;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.RANDOM_PASSWORD_BODY_3;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.RANDOM_PASSWORD_SUBJECT;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.RESET;
+import static com.example.taskmanagementapp.constants.security.SecurityConstants.RESET_PATH;
+
+import com.example.taskmanagementapp.exceptions.notfoundexceptions.ActionNotFoundException;
+import com.example.taskmanagementapp.security.utils.EmailLinkParameterProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordEmailService extends EmailService {
+    private final EmailLinkParameterProvider emailLinkParameterProvider;
+    @Value(RESET_PATH)
+    private String resetPath;
+    @Value(CONFIRMATION_PATH)
+    private String confirmationPath;
+    @Value(RANDOM_PASSWORD_REDIRECT_LINK)
+    private String redirectPath;
+
+    public PasswordEmailService(EmailLinkParameterProvider emailLinkParameterProvider) {
+        this.emailLinkParameterProvider = emailLinkParameterProvider;
+    }
+
+    public void sendActionMessage(String toEmail, String action) {
+        switch (action) {
+            case RESET -> this.sendMessage(toEmail, INITIATE_RANDOM_PASSWORD_SUBJECT,
+                    formTextForAction(toEmail, INITIATE_RANDOM_PASSWORD_BODY, resetPath));
+            case CONFIRMATION -> this.sendMessage(toEmail, CONFIRM_REGISTRATION_SUBJECT,
+                    formTextForAction(toEmail, CONFIRM_REGISTRATION_BODY, confirmationPath));
+            default -> throw new ActionNotFoundException("Unknown action " + action);
+        }
+    }
+
+    public void sendResetPassword(String toEmail, String randomPassword) {
+        this.sendMessage(toEmail, RANDOM_PASSWORD_SUBJECT,
+                    RANDOM_PASSWORD_BODY
+                            + System.lineSeparator()
+                            + System.lineSeparator()
+                            + randomPassword
+                            + System.lineSeparator()
+                            + System.lineSeparator()
+                            + RANDOM_PASSWORD_BODY_2
+                            + System.lineSeparator()
+                            + redirectPath
+                            + System.lineSeparator()
+                            + RANDOM_PASSWORD_BODY_3);
+    }
+
+    private String formTextForAction(String toEmail, String body, String actionPath) {
+        emailLinkParameterProvider.formRandomParamTokenPair(toEmail);
+        return body + System.lineSeparator() + actionPath
+                + emailLinkParameterProvider.getEmailLinkParameter()
+                + SPLITERATOR + emailLinkParameterProvider.getToken();
+    }
+}
