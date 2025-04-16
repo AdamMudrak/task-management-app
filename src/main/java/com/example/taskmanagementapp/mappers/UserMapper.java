78138dbdfbe1409dbe90f7bc0fb7e453a33c5b39
Adam Mudrak <mudrets2000@gmail.com>
2025-04-16 11:40:41 +0300

    Add empty mapper

---
+++ src/main/java/com/example/taskmanagementapp/mappers/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.taskmanagementapp.mappers;
+
+import com.example.taskmanagementapp.config.MapperConfig;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+}
