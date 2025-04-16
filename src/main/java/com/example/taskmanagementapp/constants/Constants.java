20cafb875d9097f03b08fad26c44c3712b8d8b48
Adam Mudrak <mudrets2000@gmail.com>
2025-04-16 15:32:27 +0300

    Add Constants

---
+++ src/main/java/com/example/taskmanagementapp/constants/Constants.java
@@ -0,0 +1,20 @@
+package com.example.taskmanagementapp.constants;
+
+public class Constants {
+
+    public static final String SPLITERATOR = "=";
+
+    public static final String INVALID_ENTITY_VALUE =
+            "One of the parameters is invalid. API will show which one";
+    public static final String AUTHORIZATION_REQUIRED = "Authorization required";
+    public static final String ACCESS_DENIED = "Access to this resource on the server is denied";
+
+    public static final String CODE_200 = "200";
+    public static final String CODE_201 = "201";
+    public static final String CODE_204 = "204";
+    public static final String CODE_400 = "400";
+    public static final String CODE_401 = "401";
+    public static final String CODE_403 = "403";
+
+    public static final String ROLE_USER = "hasRole('ROLE_USER')";
+}
