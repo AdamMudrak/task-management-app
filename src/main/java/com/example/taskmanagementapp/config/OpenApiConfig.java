f8f99246dde9cc4fedb6384fd572a702d1cd50b2
Adam Mudrak <mudrets2000@gmail.com>
2025-04-22 22:09:08 +0300

    Delete components not required - manage auth via Cookies automatically

--- src/main/java/com/example/taskmanagementapp/config/OpenApiConfig.java
+++ src/main/java/com/example/taskmanagementapp/config/OpenApiConfig.java
@@ -2,11 +2,7 @@ package com.example.taskmanagementapp.config;
 
 import static com.example.taskmanagementapp.constants.security.SecurityConstants.SERVER_PATH;
 
-import com.example.taskmanagementapp.constants.config.ConfigConstants;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,15 +14,6 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenApi() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(ConfigConstants.SECURITY_SCHEME_KEY,
-                            new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme(ConfigConstants.SECURITY_SCHEME)
-                                .bearerFormat(ConfigConstants.BEARER_FORMAT)))
-                                .addSecurityItem(new SecurityRequirement()
-                                .addList(ConfigConstants.SECURITY_SCHEME_KEY))
-                                .addServersItem(new Server().url(serverPath));
+        return new OpenAPI().addServersItem(new Server().url(serverPath));
     }
 }
