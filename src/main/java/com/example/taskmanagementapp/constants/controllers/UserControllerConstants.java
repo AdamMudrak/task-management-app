733a13c911b0c26f39592eb0f5875431bd9fb1c6
Adam Mudrak <mudrets2000@gmail.com>
2025-04-26 01:32:19 +0300

    Add UserControllerConstants

---
+++ src/main/java/com/example/taskmanagementapp/constants/controllers/UserControllerConstants.java
@@ -0,0 +1,30 @@
+package com.example.taskmanagementapp.constants.controllers;
+
+public class UserControllerConstants {
+    public static final String USER_API_NAME = "User API";
+    public static final String USER_API_DESCRIPTION = """
+            Here you'll find a comprehensive overview
+            of how to assign roles to users in this app
+            having SUPERVISOR role, how to get and update
+            your profile info
+            """;
+    public static final String USERS = "/users";
+    public static final String GET_PROFILE_INFO = "/me";
+    public static final String UPDATE_PROFILE_INFO = "/me";
+    public static final String UPDATE_USER_ROLE = "/{employeeId}/role";
+
+    public static final String GET_PROFILE_INFO_SUMMARY = "Retrieve profile info";
+    public static final String SUCCESSFULLY_RETRIEVED = "Successfully retrieved profile info";
+
+    public static final String UPDATE_PROFILE_INFO_SUMMARY = "Update profile info";
+    public static final String SUCCESSFULLY_UPDATED_PROFILE_INFO =
+            "Successfully updated profile info";
+
+    public static final String UPDATE_USER_ROLE_SUMMARY = "Update user role";
+    public static final String SUCCESSFULLY_UPDATED_ROLE = "Successfully updated user role";
+
+    public static final String PAGEABLE_EXAMPLE = """
+            {"page": 0,
+            "size": 5,
+             "sort": "email,DESC"}""";
+}
