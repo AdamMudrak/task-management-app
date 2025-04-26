9e84e7819f62b382b35b23635969a52483dca81c
Adam Mudrak <mudrets2000@gmail.com>
2025-04-27 00:17:53 +0300

    Add Date validators

--- src/main/java/com/example/taskmanagementapp/dtos/project/request/UpdateProjectDto.java
+++ src/main/java/com/example/taskmanagementapp/dtos/project/request/UpdateProjectDto.java
@@ -1,11 +1,14 @@
 package com.example.taskmanagementapp.dtos.project.request;
 
 import com.example.taskmanagementapp.constants.dtos.ProjectDtoConstants;
+import com.example.taskmanagementapp.validation.date.Date;
+import com.example.taskmanagementapp.validation.date.StartDateBeforeEndDate;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@StartDateBeforeEndDate
 public record UpdateProjectDto(@Schema(name = ProjectDtoConstants.NAME,
                                         example = ProjectDtoConstants.NAME_EXAMPLE)
                                String name,
@@ -14,8 +17,10 @@ public record UpdateProjectDto(@Schema(name = ProjectDtoConstants.NAME,
                                String description,
                                @Schema(name = ProjectDtoConstants.START_DATE,
                                        example = ProjectDtoConstants.START_DATE_EXAMPLE)
+                               @Date
                                LocalDate startDate,
                                @Schema(name = ProjectDtoConstants.END_DATE,
                                        example = ProjectDtoConstants.END_DATE_EXAMPLE)
+                               @Date
                                LocalDate endDate,
                                Long ownerId) {}
