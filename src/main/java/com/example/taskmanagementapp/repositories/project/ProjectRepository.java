d0a29f2ace957f05f21e25205541d628081f548d
Adam Mudrak <mudrets2000@gmail.com>
2025-04-27 01:06:43 +0300

    Add native SQL implementation of repo function

--- src/main/java/com/example/taskmanagementapp/repositories/project/ProjectRepository.java
+++ src/main/java/com/example/taskmanagementapp/repositories/project/ProjectRepository.java
@@ -3,9 +3,14 @@ package com.example.taskmanagementapp.repositories.project;
 import com.example.taskmanagementapp.entities.Project;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    //TODO HOW???
+    @Query(value = "SELECT * FROM project p "
+            + "WHERE EXISTS ("
+            + "SELECT FROM project_employees pe "
+            + "WHERE pe.project_id = p.id "
+            + "AND pe.employee_id = :employeeId", nativeQuery = true)
     List<Project> findByEmployeeId(Long employeeId);
 
     List<Project> findByOwnerId(Long ownerId);
