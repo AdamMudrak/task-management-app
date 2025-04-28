9a18a7bc0e16bf514808721e816604671e55dfba
Adam Mudrak <mudrets2000@gmail.com>
2025-04-28 12:48:15 +0300

    No support but confirmEmail needed with no auth - apply logical change

--- src/main/java/com/example/taskmanagementapp/constants/config/ConfigConstants.java
+++ src/main/java/com/example/taskmanagementapp/constants/config/ConfigConstants.java
@@ -8,7 +8,7 @@ public class ConfigConstants {
     public static final String ALLOWED_HEADERS = "*";
 
     public static final String AUTH_MATCHER = "/auth/**";
-    public static final String SUPPORT_MATCHER = "/support/**";
+    public static final String USERS_CONFIRM_EMAIL_CHANGE_MATCHER = "/users/change-email-success";
     public static final String SWAGGER_MATCHER = "/swagger-ui/**";
     public static final String SWAGGER_DOCS_MATCHER = "/v3/api-docs/**";
     public static final String ERRORS_MATCHER = "/errors";
