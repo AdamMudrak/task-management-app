2a4a6da415661cb34fcc11e1c7e70810eb23efda
Adam Mudrak <mudrets2000@gmail.com>
2025-04-30 14:51:17 +0300

    Delete unneeded constants

--- src/main/java/com/example/taskmanagementapp/constants/Constants.java
+++ src/main/java/com/example/taskmanagementapp/constants/Constants.java
@@ -7,14 +7,12 @@ public class Constants {
     public static final String INVALID_ENTITY_VALUE =
             "One of the parameters is invalid. API will show which one";
     public static final String AUTHORIZATION_REQUIRED = "Authorization required";
-    public static final String ACCESS_DENIED = "Access to this resource on the server is denied";
 
     public static final String CODE_200 = "200";
     public static final String CODE_201 = "201";
     public static final String CODE_204 = "204";
     public static final String CODE_400 = "400";
     public static final String CODE_401 = "401";
-    public static final String CODE_403 = "403";
 
     public static final String ROLE_EMPLOYEE = "hasRole('ROLE_EMPLOYEE')";
     public static final String ROLE_MANAGER = "hasRole('ROLE_MANAGER')";
