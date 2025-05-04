0d5a7f4caff43d7694ceafec2fb33d690718ed66
Adam Mudrak <mudrets2000@gmail.com>
2025-05-04 21:00:27 +0300

    Change roles names and their quantity

--- src/main/java/com/example/taskmanagementapp/constants/Constants.java
+++ src/main/java/com/example/taskmanagementapp/constants/Constants.java
@@ -14,9 +14,8 @@ public class Constants {
     public static final String CODE_400 = "400";
     public static final String CODE_401 = "401";
 
-    public static final String ROLE_EMPLOYEE = "hasRole('ROLE_EMPLOYEE')";
-    public static final String ROLE_MANAGER = "hasRole('ROLE_MANAGER')";
-    public static final String ROLE_SUPERVISOR = "hasRole('ROLE_SUPERVISOR')";
+    public static final String ROLE_USER = "hasRole('ROLE_USER')";
+    public static final String ROLE_ADMIN = "hasRole('ROLE_ADMIN')";
 
     public static final String GREEN = "\033[0;32m";// GREEN
     public static final String RESET = "\033[0m";//Reset
