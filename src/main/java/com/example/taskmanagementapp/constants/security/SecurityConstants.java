4940e41279edd6ee25c8d81be4093edc915808c8
Adam Mudrak <mudrets2000@gmail.com>
2025-05-04 23:17:17 +0300

    Add constants connected to invite logic

--- src/main/java/com/example/taskmanagementapp/constants/security/SecurityConstants.java
+++ src/main/java/com/example/taskmanagementapp/constants/security/SecurityConstants.java
@@ -1,5 +1,7 @@
 package com.example.taskmanagementapp.constants.security;
 
+import java.util.List;
+
 public class SecurityConstants {
     public static final int DIVIDER = 1000;
     public static final String SPLITERATOR = "&";
@@ -20,6 +22,8 @@ public class SecurityConstants {
     public static final String RESET_PATH = "${get.random.password.path}";
     public static final String CONFIRMATION_PATH = "${registration.confirmation.path}";
     public static final String CHANGE_EMAIL_CONFIRMATION_PATH = "${change.email.confirmation.path}";
+    public static final String ACCEPT_INVITATION_TO_PROJECT_PATH =
+            "${accept.invitation.to.project.path}";
 
     public static final String DROPBOX_REFRESH_TOKEN = "${dropbox.refresh.token}";
     public static final String DROPBOX_KEY = "${dropbox.key}";
@@ -81,7 +85,25 @@ public class SecurityConstants {
             request of email change for your Facio account. Please,
             use this link to confirm:""";
     public static final String NEW_EMAIL_PARAMETER = "&newEmail";
+    public static final String PROJECT_ID_PARAMETER = "&projectId";
+    public static final String ASSIGNEE_ID_PARAMETER = "&assigneeId";
+    public static final String IS_NEW_MANAGER_PARAMETER = "&isNewManager";
+    public static final String NEW_EMAIL = "newEmail";
+    public static final String PROJECT_ID = "projectId";
+    public static final String ASSIGNEE_ID = "assigneeId";
+    public static final String IS_NEW_MANAGER = "isNewManager";
+    public static final List<String> SKIPPED_PARAMS =
+            List.of("newEmail", "projectId", "assigneeId", "isNewManager");
 
     public static final String CONFIRM_NEW_EMAIL_MESSAGE =
             "To confirm email change, please click the link sent to your new email.";
+
+    public static final String ACCEPT_PROJECT_INVITATION_SUBJECT =
+            "Accept invitation to a project in Facio";
+    public static final String ACCEPT_PROJECT_INVITATION_BODY_1 = """
+            Good day! This email was sent to you by""";
+    public static final String ACCEPT_PROJECT_INVITATION_BODY_2 = """
+             to invite to a project called""";
+    public static final String ACCEPT_PROJECT_INVITATION_BODY_3 = """
+            . Please,use this link to confirm:""";
 }
