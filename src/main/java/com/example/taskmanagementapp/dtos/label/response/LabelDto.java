b60fb1333e26258862e3ffd30797d5249016d385
Adam Mudrak <mudrets2000@gmail.com>
2025-05-06 00:45:44 +0300

    Use class in LabelDto

--- src/main/java/com/example/taskmanagementapp/dtos/label/response/LabelDto.java
+++ src/main/java/com/example/taskmanagementapp/dtos/label/response/LabelDto.java
@@ -1,3 +1,18 @@
 package com.example.taskmanagementapp.dtos.label.response;
 
-public record LabelDto(){}
+import com.example.taskmanagementapp.dtos.comment.request.ColorDto;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LabelDto {
+    private Long id;
+    private String name;
+    private ColorDto colorDto;
+    private Long userId;
+    private Set<Long> taskIds = new HashSet<>();
+}
+
