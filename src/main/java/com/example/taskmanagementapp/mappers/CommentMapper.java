72a427e698c021e5904165b5c65cfab8daab90bb
Adam Mudrak <mudrets2000@gmail.com>
2025-05-05 15:54:02 +0300

    Add forgotten mapping to CommentMapper

--- src/main/java/com/example/taskmanagementapp/mappers/CommentMapper.java
+++ src/main/java/com/example/taskmanagementapp/mappers/CommentMapper.java
@@ -6,11 +6,14 @@ import com.example.taskmanagementapp.dtos.comment.response.CommentDto;
 import com.example.taskmanagementapp.entities.Comment;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface CommentMapper {
     Comment toAddComment(AddCommentDto commentDto);
 
+    @Mapping(target = "taskId", source = "task.id")
+    @Mapping(target = "userId", source = "user.id")
     CommentDto toCommentDto(Comment comment);
 
     List<CommentDto> toCommentDtoList(List<Comment> comments);
