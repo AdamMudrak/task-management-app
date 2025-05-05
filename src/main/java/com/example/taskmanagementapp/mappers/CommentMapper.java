f776b7291f518caebe964e1868c1a16397710536
Adam Mudrak <mudrets2000@gmail.com>
2025-05-05 13:55:13 +0300

    Add CommentMapper

---
+++ src/main/java/com/example/taskmanagementapp/mappers/CommentMapper.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagementapp.mappers;
+
+import com.example.taskmanagementapp.config.MapperConfig;
+import com.example.taskmanagementapp.dtos.comment.request.AddCommentDto;
+import com.example.taskmanagementapp.dtos.comment.response.CommentDto;
+import com.example.taskmanagementapp.entities.Comment;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface CommentMapper {
+    Comment toAddComment(AddCommentDto commentDto);
+
+    CommentDto toCommentDto(Comment comment);
+}
