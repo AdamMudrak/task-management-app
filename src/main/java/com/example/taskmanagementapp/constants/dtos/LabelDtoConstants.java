328f233924010aad1ccfa9605967897c3bd41a5a
Adam Mudrak <mudrets2000@gmail.com>
2025-05-06 00:47:13 +0300

    Add Constants for LabelDtos

---
+++ src/main/java/com/example/taskmanagementapp/constants/dtos/LabelDtoConstants.java
@@ -0,0 +1,7 @@
+package com.example.taskmanagementapp.constants.dtos;
+
+public class LabelDtoConstants {
+    public static final String NAME = "name";
+    public static final String NAME_EXAMPLE = "Best label";
+    public static final String TASK_ID = "taskId";
+}
