d48abf43a0e47069af2ce2e3a1429562ac70af8e
Adam Mudrak <mudrets2000@gmail.com>
2025-05-24 01:51:25 +0300

    Delete dead constant

--- src/main/java/com/example/taskmanagementapp/constants/dtos/LabelDtoConstants.java
+++ src/main/java/com/example/taskmanagementapp/constants/dtos/LabelDtoConstants.java
@@ -3,5 +3,4 @@ package com.example.taskmanagementapp.constants.dtos;
 public class LabelDtoConstants {
     public static final String NAME = "name";
     public static final String NAME_EXAMPLE = "Best label";
-    public static final String TASK_ID = "taskId";
 }
