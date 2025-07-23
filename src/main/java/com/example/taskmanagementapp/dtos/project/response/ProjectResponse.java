5a35a0673418bf6bc8a0e4942eff1ecaade8c236
AdamMudrak <mudrets2000@gmail.com>
2025-07-23 21:02:19 +0300

    Fix logical mistake of mapping

--- src/main/java/com/example/taskmanagementapp/dtos/project/response/ProjectResponse.java
+++ src/main/java/com/example/taskmanagementapp/dtos/project/response/ProjectResponse.java
@@ -1,7 +1,7 @@
 package com.example.taskmanagementapp.dtos.project.response;
 
 import com.example.taskmanagementapp.dtos.project.request.ProjectStatusDto;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +12,8 @@ public final class ProjectResponse {
     private Long id;
     private String name;
     private String description;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private ProjectStatusDto statusDto;
     private Long ownerId;
     private Set<Long> employeeIds;
