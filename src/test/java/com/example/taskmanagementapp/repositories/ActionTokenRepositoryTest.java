fdf490294fe6b7e29599f6231062cf9133c062ba
AdamMudrak <mudrets2000@gmail.com>
2025-07-29 00:51:38 +0300

    Make ActionTokenRepositoryTest fully self-sufficient

--- src/test/java/com/example/taskmanagementapp/repositories/ActionTokenRepositoryTest.java
+++ src/test/java/com/example/taskmanagementapp/repositories/ActionTokenRepositoryTest.java
@@ -1,8 +1,8 @@
 package com.example.taskmanagementapp.repositories;
 
 import com.dropbox.core.v2.DbxClientV2;
-import com.example.taskmanagementapp.testutils.Constants;
-import com.example.taskmanagementapp.testutils.ObjectFactory;
+import com.example.taskmanagementapp.entities.ActionToken;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,31 +14,34 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ActionTokenRepositoryTest {
+    private static final String ACTION_TOKEN = UUID.randomUUID().toString();
+    private static final String NOT_EXISTING_ACTION_TOKEN = UUID.randomUUID().toString();
     @MockitoBean
     private final DbxClientV2 dbxClientV2 = null; //unused since not needed
     @Autowired
     private ActionTokenRepository actionTokenRepository;
-
+    
     @BeforeEach
     void setUp() {
-        actionTokenRepository.save(ObjectFactory.getActionToken());
+        ActionToken actionToken = ActionToken.builder().actionToken(ACTION_TOKEN).build();
+        actionTokenRepository.save(actionToken);
     }
 
     @Test
     void givenActionToken_whenExistsByActionToken_thenReturnTrue() {
-        Assertions.assertTrue(actionTokenRepository.existsByActionToken(Constants.ACTION_TOKEN));
+        Assertions.assertTrue(actionTokenRepository.existsByActionToken(ACTION_TOKEN));
     }
 
     @Test
     void givenDeletedActionToken_whenExistsByActionToken_thenReturnFalse() {
-        Assertions.assertTrue(actionTokenRepository.existsByActionToken(Constants.ACTION_TOKEN));
-        actionTokenRepository.deleteByActionToken(Constants.ACTION_TOKEN);
-        Assertions.assertFalse(actionTokenRepository.existsByActionToken(Constants.ACTION_TOKEN));
+        Assertions.assertTrue(actionTokenRepository.existsByActionToken(ACTION_TOKEN));
+        actionTokenRepository.deleteByActionToken(ACTION_TOKEN);
+        Assertions.assertFalse(actionTokenRepository.existsByActionToken(ACTION_TOKEN));
     }
 
     @Test
     void givenNotExistingActionToken_whenExistsByActionToken_thenReturnFalse() {
         Assertions.assertFalse(
-                actionTokenRepository.existsByActionToken(Constants.NOT_EXISTING_ACTION_TOKEN));
+                actionTokenRepository.existsByActionToken(NOT_EXISTING_ACTION_TOKEN));
     }
 }
